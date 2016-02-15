1c96021c5c7c02355b1a834f7c6d5b1f4f45ee6f
Alfredo Morgado <alfredo.mor9ado@gmail.com>
2016-02-15 12:29:13 -0500

    create folders Contracts, DAL and Models - WebUI from previous version

---
+++ IPMRVPark/IPMRVPark.DAL/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace IPMRVPark.DAL.Data
+{
+    class DataContext : DbContext
+    {
+        public DataContext() : base("ipmrvparkDbContext")
+        {
+        }
+    }
+}
---
+++ IPMRVPark/IPMRVPark/Controllers/CustomersController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IPMRVPark.WebUI.Models;
+
+namespace IPMRVPark.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ipmrvparkDbContext db = new ipmrvparkDbContext();
+
+        //
+        // GET: /Customers/
+
+        public ActionResult Index()
+        {
+            return View(db.customer_view.ToList());
+        }
+
+        //
+        // GET: /Customers/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            customer_view customer_view = db.customer_view.Find(id);
+            if (customer_view == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer_view);
+        }
+
+        //
+        // GET: /Customers/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Customers/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(customer_view customer_view)
+        {
+            if (ModelState.IsValid)
+            {
+                db.customer_view.Add(customer_view);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer_view);
+        }
+
+        //
+        // GET: /Customers/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            customer_view customer_view = db.customer_view.Find(id);
+            if (customer_view == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer_view);
+        }
+
+        //
+        // POST: /Customers/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(customer_view customer_view)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer_view).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer_view);
+        }
+
+        //
+        // GET: /Customers/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            customer_view customer_view = db.customer_view.Find(id);
+            if (customer_view == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer_view);
+        }
+
+        //
+        // POST: /Customers/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            customer_view customer_view = db.customer_view.Find(id);
+            db.customer_view.Remove(customer_view);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
