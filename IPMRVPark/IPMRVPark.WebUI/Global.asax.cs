fc7a883f50366cee874700d2d3de2f2102d09f30
Alfredo Morgado <alfredo.mor9ado@gmail.com>
2016-02-16 10:13:32 -0500

    configure dependency injection : http://www.asp.net/mvc/overview/older-versions/hands-on-labs/aspnet-mvc-4-dependency-injection

--- IPMRVPark/IPMRVPark.WebUI/Global.asax.cs
+++ IPMRVPark/IPMRVPark.WebUI/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using IPMRVPark.WebUI;
 
 namespace IPMRVPark
 {
@@ -23,6 +24,9 @@ namespace IPMRVPark
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            Bootstrapper.Initialise();
+
         }
     }
 }
