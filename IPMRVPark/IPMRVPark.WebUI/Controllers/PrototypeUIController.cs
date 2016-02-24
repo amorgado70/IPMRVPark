043dd3f09ce8e3ffcada508214f971a87aa69a0b
Alfredo Morgado <alfredo.mor9ado@gmail.com>
2016-02-23 20:22:36 -0500

    reservationorderindex page

--- IPMRVPark/IPMRVPark.WebUI/Controllers/PrototypeUIController.cs
+++ IPMRVPark/IPMRVPark.WebUI/Controllers/PrototypeUIController.cs
@@ -11,7 +11,7 @@ namespace IPMRVPark.WebUI.Controllers
         //
         // GET: /PrototypeUI/
 
-        public ActionResult ReservationOrder()
+        public ActionResult ReservationOrderIndex()
         {
             return View();
         }
