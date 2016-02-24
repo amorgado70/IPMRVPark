e8f3d558d2718c7c3bbde0561dfcf76aa7de70bd
Alfredo Morgado <alfredo.mor9ado@gmail.com>
2016-02-23 19:19:17 -0500

    add boostrap css

---
+++ IPMRVPark/IPMRVPark.WebUI/Controllers/PrototypeUIController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPMRVPark.WebUI.Controllers
+{
+    public class PrototypeUIController : Controller
+    {
+        //
+        // GET: /PrototypeUI/
+
+        public ActionResult ReservationOrder()
+        {
+            return View();
+        }
+
+    }
+}
